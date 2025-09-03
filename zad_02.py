298c629ea4304b5052cadcd147ede6b34be87f05
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-03 20:44:04 +0200

    Update zad_02.py

--- zad_02.py
+++ zad_02.py
@@ -15,4 +15,24 @@ b = 8
 p = a * b
 
 
+# Koristiti print funkciju koja ce ispisati ovu poruku:
 # Povrsina cetverokuta stranica: a = [vrijednost varijable]; b = [vrijednost varijable] je: [vrijednost varijable].
+print(f'Povrsina cetverokuta stranica: a = {a}; b = {b} je: {p}.') # trenutno zanemarite ovo je samo za provjeru rjesenja
+
+# Kreirate privremene varijable koje sluze za pripremu prikaza poruke korisniku
+first_part = 'Povrsina cetverokuta stranica: a = '
+second_part = '; b = '
+third_part = ' je: '
+fourth_part = '.'
+
+print(first_part, a, second_part, b, third_part, p, fourth_part, sep='')
+
+
+# Unutar print() funkcije mozemo raditi i racunanje
+print('Povrsina cetverokuta stranica: a = ', a, '; b = ', b, ' je: ', a * b, '.', sep='')
+
+
+
+# Primjer kolege Denis Šragalj
+print('Povrsina cetverokuta stranica: a = ', a, '; b = ', b, ' je: ', p, '.', sep='')
+
