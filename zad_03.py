6ff0b64f5776918fd7c56ae0cef6bfd8aa018a0c
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-03 20:48:26 +0200

    Created zad_03.py

---
+++ zad_03.py
@@ -0,0 +1,8 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) 
+te ispišite na ekran odgovarajuće vrijednosti, za:
+    Izračun mjesečne potrošnje el. struje te cijene el. struje koju potroši 
+    mikrovalna pećnica snage 1,3 kW ako se koristi 2 sata dnevno?
+    
+    Stranice trokuta, površinu trokuta (P = (𝑎 ∗ 𝑣_𝑎)/2, 𝑣_𝑎 je visina na stranicu a) te opseg trokuta.
+'''
