d14c9f04234e7d9557baf8ad1850c7be6560f758
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-03 20:26:19 +0200

    Created zad_02.py

--- zad_01.py
+++ zad_01.py
@@ -1,15 +1,7 @@
 '''
 Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) 
 te ispišite na ekran odgovarajuće vrijednosti, za:
-    
     Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
-    
-    Stranice a i b, četverokuta te za površinu tog četverokuta.
-    
-    Izračun mjesečne potrošnje el. struje te cijene el. struje koju potroši 
-    mikrovalna pećnica snage 1,3 kW ako se koristi 2 sata dnevno?
-    
-    Stranice trokuta, površinu trokuta (P = (𝑎 ∗ 𝑣_𝑎)/2, 𝑣_𝑎 je visina na stranicu a) te opseg trokuta.
 '''
 
 first_name = 'Pero'
---
+++ zad_02.py
@@ -0,0 +1,18 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) 
+te ispišite na ekran odgovarajuće vrijednosti, za:
+    Stranice a i b, četverokuta te za površinu tog četverokuta.
+    
+    Izračun mjesečne potrošnje el. struje te cijene el. struje koju potroši 
+    mikrovalna pećnica snage 1,3 kW ako se koristi 2 sata dnevno?
+    
+    Stranice trokuta, površinu trokuta (P = (𝑎 ∗ 𝑣_𝑎)/2, 𝑣_𝑎 je visina na stranicu a) te opseg trokuta.
+'''
+
+a = 5
+b = 8
+
+p = a * b
+
+
+# Povrsina cetverokuta stranica: a = [vrijednost varijable]; b = [vrijednost varijable] je: [vrijednost varijable].
