46f328c80ecf8109ad5b2dbfd3eca2de1bf9d0d4
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 16:10:13 +0200

    Create zad_05.py

---
+++ zad_05.py
@@ -0,0 +1,9 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) te ispišite na ekran odgovarajuće vrijednosti, za:
+Ako automobil troši 5.3 litara na 100 km i ako je cijena goriva 9.56 kn po litri (nije važno kojeg goriva), izračunajte koliko košta 1 km vožnje automobilom. Prikažite mjesečni trošak (30 dana) odlaska na posao automobilom koji je udaljen 20 km u jednom smjeru.
+Imate 10000 kn i možete zaboraviti na njih na 15 godina. Ako Vam banka nudi 2.5% godišnju kamatu za taj iznos, koliko ćete zaraditi nakon 15 godina. Jednostavni kamatni račun k = C * p * t
+k = iznos kamata odnosno prinos
+C = iznos glavnice
+p = godišnja kamatna stopa – NAPOMENA: 5% = 5 / 100 = 0.05
+t = vrijeme u godinama
+'''
