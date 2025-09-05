47a6119c85ce07ccdae69db2a40755a0f7014064
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 16:10:53 +0200

    Create zad_06.py

---
+++ zad_06.py
@@ -0,0 +1,11 @@
+'''
+Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) te ispišite na ekran odgovarajuće vrijednosti, za:
+
+    Imate 10000 kn i možete zaboraviti na njih na 15 godina. 
+    Ako Vam banka nudi 2.5% godišnju kamatu za taj iznos, koliko ćete zaraditi nakon 15 godina. 
+    Jednostavni kamatni račun k = C * p * t
+        k = iznos kamata odnosno prinos
+        C = iznos glavnice
+        p = godišnja kamatna stopa – NAPOMENA: 5% = 5 / 100 = 0.05
+        t = vrijeme u godinama
+'''
