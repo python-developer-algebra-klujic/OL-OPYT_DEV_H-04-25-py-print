b8c5f71c90cd5b021102668c570a45b7eafc47f8
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 16:15:26 +0200

    Update zad_06.py

--- zad_06.py
+++ zad_06.py
@@ -9,3 +9,16 @@ Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) te is
         p = godišnja kamatna stopa – NAPOMENA: 5% = 5 / 100 = 0.05
         t = vrijeme u godinama
 '''
+
+# Ulazni podaci
+principal = 10000           # kn (glavnica)
+interest_rate = 2.5 / 100   # godisnja stopa
+time_years = 15             # godina
+
+# Izracuni
+interest = principal * interest_rate * time_years
+final_amount = principal + interest
+
+# Ispis
+print('Zarada od kamata (kn):', round(interest, 2))
+print('Ukupni iznos nakon 15 godina (kn):', round(final_amount, 2))
